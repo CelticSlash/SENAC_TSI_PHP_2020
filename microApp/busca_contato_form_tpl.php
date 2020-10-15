218215f95d8fa97c9c7b5132e0514e758c883c9c
CelticSlash <37378603+CelticSlash@users.noreply.github.com>
2020-10-15 14:35:01 -0300

    MicroApp com busca de contato

---
+++ microApp/busca_contato_form_tpl.php
@@ -0,0 +1,12 @@
+<html>
+	<head></head>
+	<body>
+		<center>
+            <h1>Buscar contato</h1>
+			<form method="post" action="buscaContato.php">
+				Buscar <input type="text" name="argumento"> 
+				<input type="submit" value="Buscar">
+			</form>
+		</center>
+	</body>
+</html>
